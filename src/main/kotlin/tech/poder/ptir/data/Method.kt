bae60fcb3642013bffa4accfbb02725e14625003
Moocow9m <moocow9m>
2021-08-26 18:40:34 -0500

    Start rewrite of internals Goals: Cleaner code, validation per method, streamlined building

--- src/main/kotlin/tech/poder/ptir/data/Method.kt
+++ src/main/kotlin/tech/poder/ptir/data/Method.kt
@@ -1,3 +1,9 @@
 package tech.poder.ptir.data
 
-data class Method(val parent: Object?, val name: String, val instructions: List<Instruction>)
+data class Method(
+    val parent: Object?,
+    val name: String,
+    val returnType: Type,
+    val instructions: List<Instruction>,
+    val args: List<Type>
+)
