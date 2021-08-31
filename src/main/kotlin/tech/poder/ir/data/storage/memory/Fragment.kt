b2d48d1f8ff47456d07d2cb247bf4cc6aaa4bef7
Moocow9m <moocow9m>
2021-08-31 09:09:30 -0500

    reserve bytes in fragment flags

--- src/main/kotlin/tech/poder/ir/data/storage/memory/Fragment.kt
+++ src/main/kotlin/tech/poder/ir/data/storage/memory/Fragment.kt
@@ -1,3 +1,43 @@
 package tech.poder.ir.data.storage.memory
 
-data class Fragment(val position: Long, val size: Int, val objectSize: Int, val freeList: MutableList<Int>)
+import kotlin.experimental.or
+
+data class Fragment(
+    val flags: Byte,
+    val position: Long,
+    val size: Int,
+    val objectSize: Int,
+    val freeList: MutableList<Int>
+) {
+    val isReadOnly by lazy {
+        flags or 2
+    }
+
+    val isExecutable by lazy {
+        flags or 1
+    }
+
+    val reservedA by lazy {
+        flags or 4
+    }
+
+    val reservedB by lazy {
+        flags or 8
+    }
+
+    val reservedC by lazy {
+        flags or 16
+    }
+
+    val reservedD by lazy {
+        flags or 32
+    }
+
+    val reservedE by lazy {
+        flags or 64
+    }
+
+    val reservedF by lazy {
+        flags or 128.toByte()
+    }
+}
