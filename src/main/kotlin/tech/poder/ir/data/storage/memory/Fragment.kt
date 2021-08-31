80498ce1fcf3970f0d538a3c33fa705e2847d6c7
Moocow9m <moocow9m>
2021-08-31 17:07:27 -0500

    update fragment state

--- src/main/kotlin/tech/poder/ir/data/storage/memory/Fragment.kt
+++ src/main/kotlin/tech/poder/ir/data/storage/memory/Fragment.kt
@@ -7,26 +7,39 @@ data class Fragment(
     val position: Long,
     val size: Int,
     val objectSize: Int,
-    var lastUsed: Int,
+    var count: Int = 0,
     val freeList: MutableList<Int> = mutableListOf(),
-    val state: State = State.EMPTY
+    var state: State = State.EMPTY
 ) {
+
+    private fun updateState() {
+        state = if (freeList.isEmpty() && count == 0) {
+            State.EMPTY
+        } else if (freeList.isEmpty() && size == (count * objectSize)) {
+            State.FULL
+        } else {
+            State.PARTIAL
+        }
+    }
 
     fun nextFree(): Int {
         return if (freeList.isEmpty()) { //use fragmented areas first!
-            lastUsed += objectSize
-            lastUsed
+            objectSize * count++
         } else {
             freeList.removeAt(0)
+        }.let {
+            updateState()
+            it
         }
     }
 
     fun free(location: Int) {
-        if (lastUsed == location) {
-            lastUsed -= objectSize
+        if (count * objectSize == location) {
+            count--
         } else {
             freeList.add(location)
         }
+        updateState()
     }
 
     val isReadOnly by lazy {
