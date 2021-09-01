5001cad5236977e8d47d6cba9d7769ef9bef2fa9
Moocow9m <moocow9m>
2021-09-01 10:31:25 -0500

    prep for windows dll parsing

--- src/test/kotlin/tech/poder/test/WindowsTest.kt
+++ src/test/kotlin/tech/poder/test/WindowsTest.kt
@@ -1,19 +1,21 @@
 package tech.poder.test
 
+import java.nio.file.FileVisitOption
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.io.path.isRegularFile
 import kotlin.test.Test
 
 class WindowsTest {
 
     @Test
     fun readDll() {
-        val dlls = Paths.get("windowsTest").toAbsolutePath()
-        Files.list(dlls).filter {
+        val dlls = Paths.get("testFiles").toAbsolutePath()
+        Files.walk(dlls, FileVisitOption.FOLLOW_LINKS).filter {
             val name = it.fileName.toString()
-            name.endsWith("dll") || name.endsWith("exe")
+            it.isRegularFile() && (name.endsWith("dll") || name.endsWith("exe"))
         }.forEach {
-
+            println(it)
         }
     }
 }
