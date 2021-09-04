9baa7c1a5c4779e3bd93b4d9ecb770ee43a8c212
Moocow9m <moocow9m>
2021-09-04 15:13:55 -0500

    Add bit manipulator

---
+++ src/main/kotlin/tech/poder/ir/util/BitManipulator.kt
@@ -0,0 +1,36 @@
+package tech.poder.ir.util
+
+import kotlin.experimental.and
+import kotlin.experimental.xor
+
+object BitManipulator {
+    val indexes: List<Long> = listOf(1, 2, 4, 8, 16, 32, 64, 128, 256)
+
+    fun Byte.isBitOn(index: Int): Boolean {
+        return !isBitOff(index)
+    }
+
+    fun Byte.isBitOff(index: Int): Boolean {
+        return this and indexes[index].toByte() == 0.toByte()
+    }
+
+    fun Byte.bitFlip(index: Int): Byte {
+        return this xor indexes[index].toByte()
+    }
+
+    fun Byte.bitOff(index: Int): Byte {
+        return if (isBitOn(index)) {
+            bitFlip(index)
+        } else {
+            this
+        }
+    }
+
+    fun Byte.bitOn(index: Int): Byte {
+        return if (isBitOff(index)) {
+            bitFlip(index)
+        } else {
+            this
+        }
+    }
+}
