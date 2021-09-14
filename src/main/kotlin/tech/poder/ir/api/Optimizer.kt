0439b30e090fe99794674b14b0eda66c3920877b
moocow9m <devmoocow9m@gmail.com>
2021-09-14 16:26:46 -0700

    partial removal of unneeded segments !broken

--- src/main/kotlin/tech/poder/ir/api/Optimizer.kt
+++ src/main/kotlin/tech/poder/ir/api/Optimizer.kt
@@ -1,37 +1,8 @@
 package tech.poder.ir.api
 
-import tech.poder.ir.data.storage.segment.*
+import tech.poder.ir.data.storage.segment.Segment
 
 interface Optimizer { //TODO add method and object visit methods... maybe
 
-    fun visitSegment(segment: Segment) {
-        when (segment) {
-            is MultiSegment -> {
-                visit(segment)
-                segment.instructions.forEach {
-                    visitSegment(it)
-                }
-            }
-            is SegmentPart -> visit(segment)
-            is LoopHolder -> {
-                visit(segment)
-                visitSegment(segment.block)
-            }
-            is BranchHolder -> {
-                visit(segment)
-                visitSegment(segment.ifBlock)
-                if (segment.elseBlock != null) {
-                    visitSegment(segment.elseBlock)
-                }
-            }
-        }
-    }
-
-    fun visit(multiSegment: MultiSegment) {}
-
-    fun visit(segmentPart: SegmentPart)
-
-    fun visit(loopHolder: LoopHolder) {}
-
-    fun visit(branchHolder: BranchHolder) {}
+    fun visitSegment(segment: Segment)
 }
