28974c7e059117b5abb56e5ae16a690e60cbb743
Moocow9m <devmoocow9m@gmail.com>
2022-01-14 12:23:55 -0800

    Add Linux builder stub

--- src/main/kotlin/tech/poder/ir/machine/LinuxBuilder.kt
+++ src/main/kotlin/tech/poder/ir/machine/LinuxBuilder.kt
@@ -1,10 +1,47 @@
 package tech.poder.ir.machine
 
-import java.io.OutputStream
+import tech.poder.proto.BitOutputStream
 
 object LinuxBuilder {
+	enum class RegistrySize {
+		M8,
+		M16,
+		M32,
+		M64,
+		M128,
+	}
+	enum class RegisterName {
+		RAX,
+		RCX,
+		RDX,
+		RBX,
+		RSP,
+		RBP,
+		RSI,
+		RDI,
+		R8,
+		R9,
+		R10,
+		R11,
+		R12,
+		R13,
+		R14,
+		R15
+	}
 	@JvmInline
-	value class AMD64(val out: OutputStream) {
+	value class AMD64(val out: BitOutputStream) {
+		fun rex(w: Boolean, r: Boolean, x: Boolean, b: Boolean) {
+			//Begin Rex Code
+			out.writeBit(false)
+			out.writeBit(true)
+			out.writeBit(false)
+			out.writeBit(false)
+			//End Rex Code
+			out.writeBit(w)
+			out.writeBit(r)
+			out.writeBit(x)
+			out.writeBit(b)
+		}
 
 	}
 }
